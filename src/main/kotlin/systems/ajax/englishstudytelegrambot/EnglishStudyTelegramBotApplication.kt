7732e8aeda2c235e009cc4ec6a5edeab5b51407c
Mykola Khlopyk <mykola.khlopyk@gmail.com>
2023-09-07 16:34:11 +0300

    initial commit

---
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplication.kt
@@ -0,0 +1,11 @@
+package systems.ajax.englishstudytelegrambot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnglishStudyTelegramBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<EnglishStudyTelegramBotApplication>(*args)
+}
---
+++ src/test/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package systems.ajax.englishstudytelegrambot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnglishStudyTelegramBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
