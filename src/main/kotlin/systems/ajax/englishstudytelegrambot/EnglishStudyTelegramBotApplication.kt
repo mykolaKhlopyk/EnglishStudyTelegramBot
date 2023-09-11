6c888b68d8b7df9d729035217bf9532f59b07ab6
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-11 15:43:30 +0300

    change generated code by spring initializr to code which detekt wanted

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplication.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class EnglishStudyTelegramBotApplication
 
-fun main(args: Array<String>) {
+fun main(vararg args: String) {
     runApplication<EnglishStudyTelegramBotApplication>(*args)
 }
--- src/test/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplicationTests.kt
+++ src/test/kotlin/systems/ajax/englishstudytelegrambot/EnglishStudyTelegramBotApplicationTests.kt
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class EnglishStudyTelegramBotApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+class EnglishStudyTelegramBotApplicationTests
