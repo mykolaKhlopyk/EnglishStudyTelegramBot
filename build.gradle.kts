28f76b8e5e81e6a4f2a58a72a86409f07c50327a
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-12 16:48:46 +0300

    remove unusable dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
-    id("io.gitlab.arturbosch.detekt") version("1.23.1")
+    id("io.gitlab.arturbosch.detekt") version ("1.23.1")
 }
 
 group = "systems.ajax"
@@ -20,9 +20,8 @@ repositories {
 }
 
 dependencies {
-   // implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
