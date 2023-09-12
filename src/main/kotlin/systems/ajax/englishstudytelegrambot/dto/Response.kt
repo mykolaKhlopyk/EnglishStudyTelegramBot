3a4cb8deca5fde04249e67801539845424aaa955
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-12 16:53:52 +0300

    change overriding toString to common interface GettingPartOfAdditionalInfoAboutWord, which declares that everyone has to return some part of info about word in String format

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/dto/Response.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/dto/Response.kt
@@ -2,31 +2,45 @@ package systems.ajax.englishstudytelegrambot.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+interface GettingPartOfAdditionalInfoAboutWord {
+
+    fun getPartOfAdditionalInfoAboutWord(): String
+
+}
+
 data class AudioForWordResponse(
     @JsonProperty("fileUrl") val fileUrl: String
-) {
-    override fun toString(): String = fileUrl
+) : GettingPartOfAdditionalInfoAboutWord {
+
+    override fun getPartOfAdditionalInfoAboutWord(): String = fileUrl
+
 }
 
 data class DefinitionOfWordResponse(
     @JsonProperty("text") val definition: String
-) {
-    override fun toString(): String = definition
+) : GettingPartOfAdditionalInfoAboutWord {
+
+    override fun getPartOfAdditionalInfoAboutWord(): String = definition
+
 }
 
 data class ExampleOfWordResponse(
     @JsonProperty("examples") val examples: Array<Example>
-) {
+) : GettingPartOfAdditionalInfoAboutWord {
+
     data class Example(@JsonProperty("text") val text: String) {
         override fun toString(): String = text
     }
 
-    override fun toString(): String =
-        buildString { examples.forEachIndexed() { index, example -> this.append("$index) $example\n") } }
+    override fun getPartOfAdditionalInfoAboutWord(): String =
+        buildString { examples.forEachIndexed { index, example -> this.append("$index) $example\n") } }
+
 }
 
 data class PronunciationOfWordResponse(
     @JsonProperty("raw") val pronunciation: String
-) {
-    override fun toString(): String = pronunciation
+) : GettingPartOfAdditionalInfoAboutWord {
+
+    override fun getPartOfAdditionalInfoAboutWord(): String = pronunciation
+
 }
