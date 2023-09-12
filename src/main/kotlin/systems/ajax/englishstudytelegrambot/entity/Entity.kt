db4612db171a57c4548fc61c9daa8c42ac1805a5
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-12 16:51:43 +0300

    update entities, made them all data classes

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -1,11 +1,12 @@
 package systems.ajax.englishstudytelegrambot.entity
 
-class Word(val spelling: String, val translate: String, val additionalInfoAboutWord: AdditionalInfoAboutWord)
+data class Word(val spelling: String, val translate: String, val additionalInfoAboutWord: AdditionalInfoAboutWord)
 
 data class AdditionalInfoAboutWord(
     val linkToAudio: String,
     val definition: String,
     val example: String,
-    val pronunciationOfWord: String)
+    val pronunciationOfWord: String
+)
 
-class Library(val name: String, val words: List<Word>)
+data class Library(val name: String, val words: List<Word>)
