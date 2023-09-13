7bf6877174136ee6b47757da226363e5612935f8
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-13 19:57:29 +0300

    replace field injection by WordnikBaseUrlProperty bean

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/config/Configuration.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/config/Configuration.kt
@@ -1,19 +1,25 @@
 package systems.ajax.englishstudytelegrambot.config
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
+import systems.ajax.englishstudytelegrambot.service.WordinkKeyProperty
+import systems.ajax.englishstudytelegrambot.service.WordnikLinkProperties
 
 @Configuration
+@EnableConfigurationProperties(WordnikLinkProperties::class, WordnikBaseUrlProperty::class, WordinkKeyProperty::class)
 class Configuration {
-
-    @Value("\${base.url.wordnikAPI}")
-    lateinit var baseUrlToWordnikAPI: String
-
     @Bean
-    fun webClient(builder: WebClient.Builder): WebClient =
+    fun webClient(builder: WebClient.Builder, wordnikBaseUrlProperty: WordnikBaseUrlProperty): WebClient =
         builder
-            .baseUrl(baseUrlToWordnikAPI)
+            .baseUrl(wordnikBaseUrlProperty.url)
             .build()
 }
+
+@ConfigurationProperties(prefix = "base")
+data class WordnikBaseUrlProperty @ConstructorBinding constructor(
+    val url: String
+)
