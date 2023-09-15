7f0c57db8780c4c86005f0cd120f4d22766c464d
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-15 10:26:51 +0300

    create new entity User, which will store info about telegram user. Add mongodb annotations

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -1,5 +1,14 @@
 package systems.ajax.englishstudytelegrambot.entity
 
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(value = "users")
+data class User(val telegramId: String, val username: String, val libraries: List<Library>)
+
+@Document(value = "libraries")
+data class Library(val id: String = ObjectId().toHexString(), val name: String, val words: List<Word>)
+
 data class Word(val spelling: String, val translate: String, val additionalInfoAboutWord: AdditionalInfoAboutWord)
 
 data class AdditionalInfoAboutWord(
@@ -8,5 +17,3 @@ data class AdditionalInfoAboutWord(
     val exampleInSentences: String,
     val pronunciationOfWord: String
 )
-
-data class Library(val name: String, val words: List<Word>)
