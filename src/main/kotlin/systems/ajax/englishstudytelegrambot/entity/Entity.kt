5662b9f493bfa73a11c1872272934685d70bf490
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-15 12:40:44 +0300

    update Library class and create LibraryRepository

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -1,13 +1,17 @@
 package systems.ajax.englishstudytelegrambot.entity
 
 import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(value = "users")
-data class User(val telegramId: String, val username: String, val libraries: List<Library>)
+data class User(@Id val telegramId: String, val username: String, val libraries: List<Library>)
 
 @Document(value = "libraries")
-data class Library(val id: String = ObjectId().toHexString(), val name: String, val words: List<Word>)
+data class Library(@Id val id: String, val name: String, val ownerId: String, val words: List<Word>) {
+
+    constructor(name: String, ownerId: String) : this(ObjectId().toHexString(), name, ownerId, mutableListOf())
+}
 
 data class Word(val spelling: String, val translate: String, val additionalInfoAboutWord: AdditionalInfoAboutWord)
 
