23e3fbee7635db521ae51f984ab7b76fe06e2e86
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-18 11:09:22 +0300

    fix in all entities parameters writing

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -5,15 +5,28 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(value = "users")
-data class User(@Id val telegramId: String, val username: String, val libraries: List<Library>)
+data class User(
+    @Id val telegramId: String,
+    val username: String,
+    val libraries: List<Library>
+)
 
 @Document(value = "libraries")
-data class Library(@Id val id: String, val name: String, val ownerId: String, val words: List<Word>) {
+data class Library(
+    @Id val id: String,
+    val name: String,
+    val ownerId: String,
+    val words: List<Word>
+) {
 
     constructor(name: String, ownerId: String) : this(ObjectId().toHexString(), name, ownerId, mutableListOf())
 }
 
-data class Word(val spelling: String, val translate: String, val additionalInfoAboutWord: AdditionalInfoAboutWord)
+data class Word(
+    val spelling: String,
+    val translate: String,
+    val additionalInfoAboutWord: AdditionalInfoAboutWord
+)
 
 data class AdditionalInfoAboutWord(
     val linkToAudio: String,
