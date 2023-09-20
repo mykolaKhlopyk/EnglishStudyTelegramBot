926037e60fe1dcc8529e5ac0a609d2e1382eb77a
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-20 11:15:48 +0300

    refactoring

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -7,8 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document(value = "users")
 data class User(@Id val telegramId: String, val username: String, val libraries: List<Library>)
 
-@Document(value = "libraries")
-data class Library(@Id val id: String, val name: String, val ownerId: String, val words: List<Word>) {
+data class Library(val name: String, val ownerId: String, val words: List<Word>) {
 
     constructor(name: String, ownerId: String) : this(ObjectId().toHexString(), name, ownerId, mutableListOf())
 }
--- src/main/kotlin/systems/ajax/englishstudytelegrambot/telegram/TelegramBot.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/telegram/TelegramBot.kt
@@ -2,12 +2,10 @@ package systems.ajax.englishstudytelegrambot.telegram
 
 import org.springframework.stereotype.Component
 
-
-
 //temporary class which will simulate future telegram bot api
 @Component
 class TelegramBot {
+
     val userId: String
         get() = "testId"
-
 }
