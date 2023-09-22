a210a262b92aa0d8b951e4a30464a05e8b27b06d
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-22 18:37:44 +0300

    refactor wordService for saving word

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/service/WordService.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/service/WordService.kt
@@ -3,32 +3,32 @@ package systems.ajax.englishstudytelegrambot.service
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import systems.ajax.englishstudytelegrambot.dto.WordDto
+import systems.ajax.englishstudytelegrambot.entity.Library
 import systems.ajax.englishstudytelegrambot.entity.Word
-import systems.ajax.englishstudytelegrambot.exception.WordNotFoundBySpendingException
+import systems.ajax.englishstudytelegrambot.repository.LibraryRepository
 import systems.ajax.englishstudytelegrambot.repository.WordRepository
 
 interface WordService {
 
-
     fun getWordByItsSpelling(wordSpelling: String): Word
-    suspend fun  saveNewWord(libraryName: String, telegramUserId: String, wordDto: WordDto): Word
+
+    suspend fun saveNewWordInLibrary(libraryName: String, telegramUserId: String, wordDto: WordDto): Library
 }
 
 @Service
 class WordServiceImpl(
-    val wordRepository: WordRepository,
+    val wordRepository: WordRepository
     val additionalInfoAboutWordService: AdditionalInfoAboutWordService
 ) : WordService {
 
-    override suspend fun saveNewWord(libraryName: String, telegramUserId: String, wordDto: WordDto): Word {
+    override suspend fun saveNewWordInLibrary(libraryName: String, telegramUserId: String, wordDto: WordDto): Library {
         val word =
             Word(
                 wordDto.spelling,
                 wordDto.translate,
                 additionalInfoAboutWordService.findAdditionInfoAboutWord(wordDto.spelling)
             )
-        log.info("full saving word {}", word)
-        return wordRepository.saveNewWord(word)
+        return wordRepository.saveNewWordInLibrary(word, libraryName, telegramUserId)
     }
 
     override fun getWordByItsSpelling(wordSpelling: String): Word =
