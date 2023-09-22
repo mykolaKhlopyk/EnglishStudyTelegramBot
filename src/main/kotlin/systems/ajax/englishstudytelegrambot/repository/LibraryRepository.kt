898f1d917bc285b0935184efd56297b8312c5aec
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-22 10:21:31 +0300

    added methods to repository layer

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/LibraryRepository.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/LibraryRepository.kt
@@ -1,6 +1,20 @@
 package systems.ajax.englishstudytelegrambot.repository
 
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.stereotype.Repository
 import systems.ajax.englishstudytelegrambot.entity.Library
 
-interface LibraryRepository : MongoRepository<Library, String>
+interface LibraryRepository {
+
+    fun saveNewLibrary(nameOfNewLibrary: String, ownerId: String): Library
+    fun getAllLibraries(): List<Library>
+}
+
+@Repository
+class LibraryRepositoryImpl(val mongoTemplate: MongoTemplate) : LibraryRepository {
+
+    override fun saveNewLibrary(nameOfNewLibrary: String, ownerId: String): Library =
+        mongoTemplate.insert(Library(name = nameOfNewLibrary, ownerId = ownerId))
+
+    override fun getAllLibraries(): List<Library> = mongoTemplate.findAll(Library::class.java)
+}
--- src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/UserRepository.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/UserRepository.kt
@@ -1,26 +1,38 @@
 package systems.ajax.englishstudytelegrambot.repository
 
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Repository
+import systems.ajax.englishstudytelegrambot.entity.Library
 import systems.ajax.englishstudytelegrambot.entity.User
 
 
 interface UserRepository {
 
-    fun getAll(): List<User>
+    fun getAllUsers(): List<User>
 
     fun insert(newUser: User): User
 
-    fun getUserByTelegramId(telegramId: String): User?
+    fun getUserByTelegramId(telegramId: String): User
+
+    fun getAllLibrariesOfUser(telegramIdOfCurrentUser: String): List<Library>
 }
 
 @Repository
 class UserRepositoryImpl(val mongoTemplate: MongoTemplate) : UserRepository {
 
-    override fun getAll(): List<User> = mongoTemplate.findAll(User::class.java)
+    override fun getAllUsers(): List<User> = mongoTemplate.findAll(User::class.java)
 
     override fun insert(newUser: User): User = mongoTemplate.insert(newUser)
 
-    override fun getUserByTelegramId(telegramId: String): User? =
-        mongoTemplate.findById(telegramId, User::class.java)
+    override fun getUserByTelegramId(telegramId: String): User =
+        mongoTemplate.findById(telegramId, User::class.java) ?: insert(User(telegramId))
+
+    override fun getAllLibrariesOfUser(telegramIdOfCurrentUser: String): List<Library> =
+        mongoTemplate.find(
+            Query.query(Criteria.where("ownerId").`is`(telegramIdOfCurrentUser)),
+            Library::class.java
+        )
 }
