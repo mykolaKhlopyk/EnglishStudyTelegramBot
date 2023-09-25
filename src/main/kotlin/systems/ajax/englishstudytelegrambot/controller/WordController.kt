487951efad441d9efbe6897051f1ef09668809d0
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-26 01:14:01 +0300

    update controller and service layers for word entity

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/controller/WordController.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/controller/WordController.kt
@@ -1,40 +1,55 @@
 package systems.ajax.englishstudytelegrambot.controller
 
-import jakarta.validation.constraints.NotBlank
-import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 import systems.ajax.englishstudytelegrambot.annotation.LogMethodsByRequiredAnnotations
 import systems.ajax.englishstudytelegrambot.dto.WordDto
-import systems.ajax.englishstudytelegrambot.entity.AdditionalInfoAboutWord
-import systems.ajax.englishstudytelegrambot.entity.Library
-import systems.ajax.englishstudytelegrambot.service.AdditionalInfoAboutWordService
+import systems.ajax.englishstudytelegrambot.entity.Word
 import systems.ajax.englishstudytelegrambot.service.WordService
 
 @RestController
 @RequestMapping("/api/word")
 @LogMethodsByRequiredAnnotations(LogMethodsByRequiredAnnotations::class, PostMapping::class)
 class WordController(
-    val additionalInfoAboutWordService: AdditionalInfoAboutWordService,
     val wordService: WordService
 ) {
 
     @LogMethodsByRequiredAnnotations
-    @GetMapping("addInfo/{word}")
-    suspend fun getAdditionalInfoAboutWord(
-        @NotBlank @PathVariable("word") wordSpelling: String
-    ): AdditionalInfoAboutWord =
-        additionalInfoAboutWordService.findAdditionInfoAboutWord(wordSpelling)
+    @GetMapping("/{libraryName}/fullInfo")
+    fun getAdditionalInfoAboutWord(
+        @PathVariable("libraryName") libraryName: String,
+        @RequestHeader("telegramUserId") telegramUserId: String,
+        @RequestHeader("wordSpelling") wordSpelling: String
+    ): Word =
+        wordService.getFullInfoAboutWord(libraryName, telegramUserId, wordSpelling)
 
     @PostMapping("/{libraryName}")
     suspend fun saveWordInLibrary(
         @PathVariable("libraryName") libraryName: String,
         @RequestHeader("telegramUserId") telegramUserId: String,
         @RequestBody wordDto: WordDto
-    ): Library = wordService.saveNewWordInLibrary(libraryName, telegramUserId, wordDto)
+    ): Word = wordService.saveNewWord(libraryName, telegramUserId, wordDto)
+
+    @PatchMapping("/{libraryName}")
+    fun updateWordTranslateInLibrary(
+        @PathVariable("libraryName") libraryName: String,
+        @RequestHeader("telegramUserId") telegramUserId: String,
+        @RequestBody wordDto: WordDto
+    ): Word = wordService.updateWordTranslate(libraryName, telegramUserId, wordDto)
+
+    @DeleteMapping("/{libraryName}")
+    fun deleteWordFromLibrary(
+        @PathVariable("libraryName") libraryName: String,
+        @RequestHeader("telegramUserId") telegramUserId: String,
+        @RequestHeader("wordSpelling") wordSpelling: String
+    ): Word = wordService.deleteWord(libraryName, telegramUserId, wordSpelling)
+
+
 }
--- src/main/kotlin/systems/ajax/englishstudytelegrambot/service/WordService.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/service/WordService.kt
@@ -1,17 +1,22 @@
 package systems.ajax.englishstudytelegrambot.service
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import systems.ajax.englishstudytelegrambot.dto.WordDto
-import systems.ajax.englishstudytelegrambot.entity.Library
 import systems.ajax.englishstudytelegrambot.entity.Word
 import systems.ajax.englishstudytelegrambot.exception.WordAlreadyPresentInLibraryException
+import systems.ajax.englishstudytelegrambot.exception.WordNotFoundBySpendingException
 import systems.ajax.englishstudytelegrambot.repository.LibraryRepository
 import systems.ajax.englishstudytelegrambot.repository.WordRepository
 
 interface WordService {
 
-    suspend fun saveNewWordInLibrary(libraryName: String, telegramUserId: String, wordDto: WordDto): Library
+    suspend fun saveNewWord(libraryName: String, telegramUserId: String, wordDto: WordDto): Word
+
+    fun updateWordTranslate(libraryName: String, telegramUserId: String, wordDto: WordDto): Word
+
+    fun deleteWord(libraryName: String, telegramUserId: String, wordSpelling: String): Word
+
+    fun getFullInfoAboutWord(libraryName: String, telegramUserId: String, wordSpelling: String): Word
 }
 
 @Service
@@ -21,28 +26,45 @@ class WordServiceImpl(
     val additionalInfoAboutWordService: AdditionalInfoAboutWordService
 ) : WordService {
 
-    override suspend fun saveNewWordInLibrary(libraryName: String, telegramUserId: String, wordDto: WordDto): Library {
-        val library = libraryRepository.getLibraryByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
-        library
-            .takeIf { isLibraryContainsWordWithSpelling(it, wordDto.spelling) }
-            ?.let {
-                val word =
-                    Word(
-                        wordDto.spelling,
-                        wordDto.translate,
-                        additionalInfoAboutWordService.findAdditionInfoAboutWord(wordDto.spelling)
-                    )
-                return wordRepository.saveNewWordInLibrary(word, it)
-            } ?: throw WordAlreadyPresentInLibraryException()
+    override suspend fun saveNewWord(libraryName: String, telegramUserId: String, wordDto: WordDto): Word {
+        val libraryId = libraryRepository.getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
+        libraryId.takeIf {
+            !wordRepository.isWordBelongsToLibraryByWordSpelling(wordDto.spelling, libraryId)
+        }?.let {
+            return wordRepository.saveNewWord(
+                Word(
+                    spelling = wordDto.spelling,
+                    translate = wordDto.translate,
+                    libraryId = libraryId,
+                    additionalInfoAboutWord = additionalInfoAboutWordService.findAdditionInfoAboutWord(wordDto.spelling)
+                )
+            )
+        } ?: throw WordAlreadyPresentInLibraryException()
     }
 
+    override fun updateWordTranslate(libraryName: String, telegramUserId: String, wordDto: WordDto): Word {
+        val libraryId = libraryRepository.getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
+        val wordId = wordRepository.getWordIdBySpellingAndLibraryId(wordDto.spelling, libraryId)
+        wordId.takeIf {
+            wordRepository.isWordBelongsToLibraryByWordId(wordId, libraryId)
+        }?.let {
+            return wordRepository.updateWordTranslating(wordId, wordDto.translate)
+        } ?: throw WordNotFoundBySpendingException()
+    }
 
-    private fun isLibraryContainsWordWithSpelling(
-        library: Library,
-        wordSpelling: String
-    ) = library.words.none { word -> word.spelling == wordSpelling }
+    override fun deleteWord(libraryName: String, telegramUserId: String, wordSpelling: String): Word {
+        val libraryId = libraryRepository.getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
+        val wordId = wordRepository.getWordIdBySpellingAndLibraryId(libraryName, telegramUserId)
+        wordId.takeIf {
+            wordRepository.isWordBelongsToLibraryByWordId(wordId, libraryId)
+        }?.let {
+            return wordRepository.deleteWord(wordSpelling)
+        } ?: throw WordNotFoundBySpendingException()
+    }
 
-    companion object {
-        val log = LoggerFactory.getLogger(this::class.java)
+    override fun getFullInfoAboutWord(libraryName: String, telegramUserId: String, wordSpelling: String): Word {
+        val libraryId = libraryRepository.getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
+        val wordId = wordRepository.getWordIdBySpellingAndLibraryId(wordSpelling, libraryId)
+        return wordRepository.getFullInfoAboutWord(wordId)
     }
 }
