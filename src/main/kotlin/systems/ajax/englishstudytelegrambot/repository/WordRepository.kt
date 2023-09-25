3fcde7613a6b2016f9c340a9a41155b8c33896f3
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-26 01:10:12 +0300

    implement WordRepository

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/WordRepository.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/WordRepository.kt
@@ -6,13 +6,27 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Repository
-import systems.ajax.englishstudytelegrambot.entity.Library
 import systems.ajax.englishstudytelegrambot.entity.Word
-import systems.ajax.englishstudytelegrambot.exception.LibraryIsMissingException
+import systems.ajax.englishstudytelegrambot.exception.WordIsMissing
+import systems.ajax.englishstudytelegrambot.exception.WordNotFoundBySpendingException
 
 interface WordRepository {
 
-    fun saveNewWordInLibrary(word: Word, library: Library): Library
+    fun saveNewWord(word: Word): Word
+
+    fun updateWordTranslating(wordId: String, newWordTranslate: String): Word
+
+    fun deleteWord(wordId: String): Word
+
+    fun isWordBelongsToLibraryByWordId(wordId: String, libraryId: String): Boolean
+
+    fun isWordBelongsToLibraryByWordSpelling(wordSpelling: String, libraryId: String): Boolean
+
+    fun getFullInfoAboutWord(wordId: String): Word
+
+    fun getWordIdBySpellingAndLibraryId(wordSpelling: String, libraryId: String): String
+
+    fun getAllWords(): List<Word>
 }
 
 @Repository
@@ -20,11 +34,46 @@ class WordRepositoryImpl(
     val mongoTemplate: MongoTemplate
 ) : WordRepository {
 
-    override fun saveNewWordInLibrary(word: Word, library: Library): Library =
+    override fun saveNewWord(word: Word): Word =
+        mongoTemplate.save(word)
+
+    override fun updateWordTranslating(wordId: String, newWordTranslate: String): Word =
         mongoTemplate.findAndModify(
-            Query.query(Criteria.where("_id").`is`(library.id)),
-            Update().push("words", word),
+            Query.query(Criteria.where("_id").`is`(wordId)),
+            Update.update("translate", newWordTranslate),
             FindAndModifyOptions().returnNew(true),
-            Library::class.java
-        ) ?: throw LibraryIsMissingException()
+            Word::class.java
+        ) ?: throw WordNotFoundBySpendingException()
+
+    override fun deleteWord(wordId: String): Word =
+        mongoTemplate.findAndRemove(
+            Query.query(Criteria.where("_id").`is`(wordId)),
+            Word::class.java
+        ) ?: throw WordNotFoundBySpendingException()
+
+    override fun isWordBelongsToLibraryByWordId(wordId: String, libraryId: String): Boolean =
+        mongoTemplate.exists(
+            Query.query(Criteria.where("libraryId").`is`(libraryId).and("id").`is`(wordId)),
+            Word::class.java
+        )
+
+    override fun isWordBelongsToLibraryByWordSpelling(wordSpelling: String, libraryId: String): Boolean =
+        mongoTemplate.exists(
+            Query.query(Criteria.where("libraryId").`is`(libraryId).and("spelling").`is`(wordSpelling)),
+            Word::class.java
+        )
+
+    override fun getFullInfoAboutWord(wordId: String): Word =
+        mongoTemplate.findById(wordId, Word::class.java) ?: throw WordIsMissing()
+
+    override fun getAllWords(): List<Word> =
+        mongoTemplate.findAll(Word::class.java)
+
+
+
+    override fun getWordIdBySpellingAndLibraryId(wordSpelling: String, libraryId: String): String =
+        mongoTemplate.findOne(
+            Query.query(Criteria.where("spelling").`is`(wordSpelling).and("libraryId").`is`(libraryId)),
+            Word::class.java
+        )?.id ?: throw WordIsMissing()
 }
