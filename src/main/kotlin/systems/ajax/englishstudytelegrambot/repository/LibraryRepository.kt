40e2837d217ad4621a81eb6ab945fe4b3638f434
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-26 01:09:47 +0300

    add two methods to LibraryRepository and implementing of them

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/LibraryRepository.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/LibraryRepository.kt
@@ -1,5 +1,6 @@
 package systems.ajax.englishstudytelegrambot.repository
 
+import org.bson.types.ObjectId
 import org.springframework.dao.DuplicateKeyException
 import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.MongoTemplate
@@ -7,7 +8,8 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
 import systems.ajax.englishstudytelegrambot.entity.Library
-import systems.ajax.englishstudytelegrambot.exception.LibraryWithTheSameNameForUserWasCreatedException
+import systems.ajax.englishstudytelegrambot.entity.Word
+import systems.ajax.englishstudytelegrambot.exception.LibraryWithTheSameNameForUserAlreadyExistException
 import systems.ajax.englishstudytelegrambot.exception.LibraryIsMissingException
 
 interface LibraryRepository {
@@ -19,6 +21,10 @@ interface LibraryRepository {
     fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): Library
 
     fun getLibraryByPairLibraryNameAndTelegramUserId(libraryName: String, telegramUserId: String): Library
+
+    fun getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName: String, telegramUserId: String): String
+
+    fun getAllWordsFromLibrary(libraryId: String): List<Word>
 }
 
 @Repository
@@ -28,7 +34,8 @@ class LibraryRepositoryImpl(val mongoTemplate: MongoTemplate) : LibraryRepositor
         try {
             mongoTemplate.insert(Library(name = nameOfNewLibrary, ownerId = telegramUserId))
         } catch (e: DuplicateKeyException) {
-            throw LibraryWithTheSameNameForUserWasCreatedException()
+            log.error("library with same name exist already {}", e.message)
+            throw LibraryWithTheSameNameForUserAlreadyExistException()
         }
 
     override fun getAllLibraries(): List<Library> = mongoTemplate.findAll(Library::class.java)
@@ -45,15 +52,30 @@ class LibraryRepositoryImpl(val mongoTemplate: MongoTemplate) : LibraryRepositor
             Library::class.java
         ) ?: throw LibraryIsMissingException()
 
-    fun queryToFindLibraryByNameAndTelegramUserId(
+    override fun getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName: String, telegramUserId: String): String =
+        (mongoTemplate.findOne(
+            queryToFindLibraryByNameAndTelegramUserId(telegramUserId, libraryName).apply {
+                this.fields().include("_id")
+            },
+            Map::class.java,
+            "libraries"
+        )?.get("_id") as? ObjectId)?.toHexString() ?: throw LibraryIsMissingException()
+
+    private fun queryToFindLibraryByNameAndTelegramUserId(
         telegramUserId: String,
         nameOfLibraryForDeleting: String
-    ) = Query.query(
+    ): Query = Query.query(
         Criteria().andOperator(
             Criteria.where("ownerId").`is`(telegramUserId),
             Criteria.where("name").`is`(nameOfLibraryForDeleting)
         )
     )
+
+    override fun getAllWordsFromLibrary(libraryId: String): List<Word> =
+        mongoTemplate.find(
+            Query.query(Criteria.where("libraryId").`is`(libraryId)),
+            Word::class.java
+        )
 
     companion object {
         val log = LoggerFactory.getLogger(this::class.java)
