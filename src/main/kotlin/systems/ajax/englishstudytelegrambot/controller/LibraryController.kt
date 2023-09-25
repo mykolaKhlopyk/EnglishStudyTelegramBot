2f8b4731c0dc475eac273b75d0c77585fc9e8731
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-26 01:12:40 +0300

    add end point to get all words from library

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/controller/LibraryController.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/controller/LibraryController.kt
@@ -1,7 +1,14 @@
 package systems.ajax.englishstudytelegrambot.controller
 
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.DeleteMapping
 import systems.ajax.englishstudytelegrambot.entity.Library
+import systems.ajax.englishstudytelegrambot.entity.Word
 import systems.ajax.englishstudytelegrambot.service.LibraryService
 
 @RestController
@@ -9,6 +16,13 @@ import systems.ajax.englishstudytelegrambot.service.LibraryService
 class LibraryController(
     val libraryService: LibraryService
 ) {
+
+    @GetMapping("/{libraryName}")
+    fun getAllWordsFromLibrary(
+        @PathVariable("libraryName") libraryName: String,
+        @RequestHeader("telegramUserId") telegramUserId: String
+    ): List<Word> =
+        libraryService.getAllWordsFromLibrary(libraryName, telegramUserId)
 
     @PostMapping("/{nameOfNewLibrary}")
     fun createLibrary(
@@ -23,5 +37,4 @@ class LibraryController(
         @RequestHeader("telegramUserId") telegramUserId: String
     ): Library =
         libraryService.deleteLibrary(nameOfLibraryForDeleting, telegramUserId)
-
 }
--- src/main/kotlin/systems/ajax/englishstudytelegrambot/service/LibraryService.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/service/LibraryService.kt
@@ -2,13 +2,17 @@ package systems.ajax.englishstudytelegrambot.service
 
 import org.springframework.stereotype.Service
 import systems.ajax.englishstudytelegrambot.entity.Library
+import systems.ajax.englishstudytelegrambot.entity.Word
 import systems.ajax.englishstudytelegrambot.repository.LibraryRepository
 import systems.ajax.englishstudytelegrambot.repository.UserRepository
 
 interface LibraryService {
+
     fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): Library
+
     fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): Library
 
+    fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): List<Word>
 }
 
 @Service
@@ -20,12 +24,12 @@ class LibraryServiceImpl(
     override fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): Library =
         libraryRepository.saveNewLibrary(nameOfNewLibrary, telegramUserId)
 
+
     override fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): Library =
         libraryRepository.deleteLibrary(nameOfLibraryForDeleting, telegramUserId)
 
-    private fun String.isUserHavingLibraryWithName(nameOfNewLibrary: String) =
-        userRepository.getAllLibrariesOfUser(this)
-            .asSequence()
-            .map(Library::name)
-            .contains(nameOfNewLibrary)
+    override fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): List<Word> {
+        val libraryId = libraryRepository.getLibraryIdByPairLibraryNameAndTelegramUserId(libraryName, telegramUserId)
+        return libraryRepository.getAllWordsFromLibrary(libraryId)
+    }
 }
