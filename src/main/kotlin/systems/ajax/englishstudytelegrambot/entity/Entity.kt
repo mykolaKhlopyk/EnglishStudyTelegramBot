cb22ffd1a867eeacfccb2502df8d6412cdab29ab
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-09-27 12:24:43 +0300

    add indexes of unique pairs

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/entity/Entity.kt
@@ -2,6 +2,7 @@ package systems.ajax.englishstudytelegrambot.entity
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(value = "users")
@@ -10,18 +11,20 @@ data class User(
 )
 
 @Document(value = "libraries")
+@CompoundIndex(name = "libraryName_owner", def = "{'name' : 1, 'ownerId': 1}", unique = true)
 data class Library(
-    @Id val id: String = ObjectId().toHexString(),
+    @Id val id: ObjectId = ObjectId(),
     val name: String,
     val ownerId: String,
 )
 
 @Document(value = "words")
+@CompoundIndex(name = "libraryId_spelling", def = "{'libraryId' : 1, 'spelling': 1}", unique = true)
 data class Word(
-    @Id val id: String = ObjectId().toHexString(),
+    @Id val id: ObjectId = ObjectId(),
     val spelling: String,
     val translate: String,
-    val libraryId: String,
+    val libraryId: ObjectId,
     val additionalInfoAboutWord: AdditionalInfoAboutWord
 )
 
