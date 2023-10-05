2bb5cbea7255d8b70423209f0c8fc336c08ca45a
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-05 11:23:46 +0300

    rewrite finding all users

--- src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/UserRepository.kt
+++ src/main/kotlin/systems/ajax/englishstudytelegrambot/repository/UserRepository.kt
@@ -1,33 +1,31 @@
 package systems.ajax.englishstudytelegrambot.repository
 
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.findDistinct
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
+import systems.ajax.englishstudytelegrambot.annotation.LogMethodsByRequiredAnnotations
 import systems.ajax.englishstudytelegrambot.entity.Library
 import systems.ajax.englishstudytelegrambot.entity.User
+import kotlin.math.log
 
 
 interface UserRepository {
 
     fun getAllUsers(): List<User>
 
-    fun insert(newUser: User): User
-
-    fun getUserByTelegramId(telegramUserId: String): User
-
     fun getAllLibrariesOfUser(telegramUserId: String): List<Library>
 }
 
 @Repository
+@LogMethodsByRequiredAnnotations(LogMethodsByRequiredAnnotations::class)
 class UserRepositoryImpl(val mongoTemplate: MongoTemplate) : UserRepository {
 
-    override fun getAllUsers(): List<User> = mongoTemplate.findAll(User::class.java)
-
-    override fun insert(newUser: User): User = mongoTemplate.insert(newUser)
-
-    override fun getUserByTelegramId(telegramUserId: String): User =
-        mongoTemplate.findById(telegramUserId, User::class.java) ?: insert(User(telegramUserId))
+    @LogMethodsByRequiredAnnotations
+    override fun getAllUsers(): List<User> =
+        mongoTemplate.find(Query().apply { fields().include("ownerId").exclude("_id") }, User::class.java, "libraries")
+            .distinct()
 
     override fun getAllLibrariesOfUser(telegramUserId: String): List<Library> =
         mongoTemplate.find(
