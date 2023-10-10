828e95c63d2ae4116f64d429be923165b2e41e28
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-10 18:21:28 +0300

    add mapper

---
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/mapper/MapperEntityToResponse.kt
@@ -0,0 +1,29 @@
+package systems.ajax.englishstudytelegrambot.nats.mapper
+
+import entity.LibraryOuterClass
+import entity.WordOuterClass
+import systems.ajax.englishstudytelegrambot.entity.Library
+import systems.ajax.englishstudytelegrambot.entity.Word
+
+fun Library.toLibraryResponse(): LibraryOuterClass.Library =
+    LibraryOuterClass.Library.newBuilder()
+        .setId(id.toHexString())
+        .setName(name)
+        .setOwnerId(ownerId)
+        .build()
+
+fun Word.toWordResponse(): WordOuterClass.Word =
+    WordOuterClass.Word.newBuilder()
+        .setId(id.toHexString())
+        .setLibraryId(libraryId.toHexString())
+        .setSpelling(spelling)
+        .setTranslating(translate)
+        .setAdditionInfoAboutWord(
+            additionalInfoAboutWord.let {
+                WordOuterClass.AdditionalInfoAboutWord.newBuilder()
+                    .setDefinitionOfWord(it.definitionOfWord)
+                    .setPronunciationOfWord(it.pronunciationOfWord)
+                    .setExampleInSentences(it.exampleInSentences)
+                    .setLinkToAudio(it.linkToAudio)
+            })
+        .build()
