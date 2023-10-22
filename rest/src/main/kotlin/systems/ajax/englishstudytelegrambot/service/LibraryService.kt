9267813f5248b8c1b0ab2f35987f1161c7cc26f7
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-22 15:16:01 +0300

    add rector functionality

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/service/LibraryService.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/service/LibraryService.kt
@@ -1,6 +1,8 @@
 package systems.ajax.englishstudytelegrambot.service
 
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import systems.ajax.englishstudytelegrambot.dto.entity.LibraryDtoResponse
 import systems.ajax.englishstudytelegrambot.dto.entity.WordDtoResponse
 import systems.ajax.englishstudytelegrambot.dto.entity.toDtoResponse
@@ -10,11 +12,11 @@ import systems.ajax.englishstudytelegrambot.repository.LibraryRepository
 
 interface LibraryService {
 
-    fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): LibraryDtoResponse
+    fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): Mono<LibraryDtoResponse>
 
-    fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): LibraryDtoResponse
+    fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): Mono<LibraryDtoResponse>
 
-    fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): List<WordDtoResponse>
+    fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): Flux<WordDtoResponse>
 }
 
 @Service
@@ -22,17 +24,26 @@ class LibraryServiceImpl(
     val libraryRepository: LibraryRepository
 ) : LibraryService {
 
-    override fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): LibraryDtoResponse =
-        libraryRepository.saveNewLibrary(nameOfNewLibrary, telegramUserId).toDtoResponse()
+    override fun createNewLibrary(nameOfNewLibrary: String, telegramUserId: String): Mono<LibraryDtoResponse> =
+        libraryRepository
+            .saveNewLibrary(nameOfNewLibrary, telegramUserId)
+            .map(Library::toDtoResponse)
 
-    override fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): LibraryDtoResponse {
-        val libraryId =
-            libraryRepository.getLibraryIdByLibraryNameAndTelegramUserId(nameOfLibraryForDeleting, telegramUserId)
-        return libraryRepository.deleteLibrary(libraryId).toDtoResponse()
-    }
+    override fun deleteLibrary(nameOfLibraryForDeleting: String, telegramUserId: String): Mono<LibraryDtoResponse> =
+        libraryRepository
+            .getLibraryIdByLibraryNameAndTelegramUserId(
+                nameOfLibraryForDeleting,
+                telegramUserId
+            )
+            .flatMap(libraryRepository::deleteLibrary)
+            .map(Library::toDtoResponse)
 
-    override fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): List<WordDtoResponse> {
-        val libraryId = libraryRepository.getLibraryIdByLibraryNameAndTelegramUserId(libraryName, telegramUserId)
-        return libraryRepository.getAllWordsFromLibrary(libraryId).map(Word::toDtoResponse)
-    }
+    override fun getAllWordsFromLibrary(libraryName: String, telegramUserId: String): Flux<WordDtoResponse> =
+        libraryRepository
+            .getLibraryIdByLibraryNameAndTelegramUserId(
+                libraryName,
+                telegramUserId
+            )
+            .flatMapMany(libraryRepository::getAllWordsFromLibrary)
+            .map(Word::toDtoResponse)
 }
