c56ec1be2bcb6e48b290d78108c11715f058d3d7
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-28 13:24:46 +0300

    add reactor-test dependency

--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation:3.1.3")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive:3.1.5")
     implementation ("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.2")
+    testImplementation("io.projectreactor:reactor-test:3.5.11")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
