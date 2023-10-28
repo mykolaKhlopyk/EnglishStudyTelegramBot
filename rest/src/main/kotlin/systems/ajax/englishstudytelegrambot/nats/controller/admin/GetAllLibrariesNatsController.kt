4500de39e945e7941e9fbb2b40c623210bd99fc0
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-28 14:21:37 +0300

    add reactor to nats controllers

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/controller/admin/GetAllLibrariesNatsController.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/controller/admin/GetAllLibrariesNatsController.kt
@@ -2,6 +2,7 @@ package systems.ajax.englishstudytelegrambot.nats.controller.admin
 
 import com.google.protobuf.Parser
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import systems.ajax.NatsSubject.Admin.GET_ALL_LIBRARIES_SUBJECT
 import systems.ajax.englishstudytelegrambot.dto.entity.LibraryDtoResponse
 import systems.ajax.entity.LibraryOuterClass.Library
@@ -20,18 +21,15 @@ class GetAllLibrariesNatsController(
 
     override val parser: Parser<GetAllLibrariesRequest> = GetAllLibrariesRequest.parser()
 
-    override fun handle(request: GetAllLibrariesRequest): GetAllLibrariesResponse =
-        runCatching {
-            val librariesResponse = getAllLibrariesInResponseFormat()
-            createSuccessResponse(librariesResponse)
-        }.getOrElse {
-            createFailureResponse(it)
-        }
+    override fun handle(request: GetAllLibrariesRequest): Mono<GetAllLibrariesResponse> =
+        getAllLibrariesInResponseFormat()
+            .map { createSuccessResponse(it) }
+            .onErrorResume { Mono.just(createFailureResponse(it)) }
 
-    private fun getAllLibrariesInResponseFormat(): List<Library> =
-        adminService.getAllLibraries().map(LibraryDtoResponse::toLibraryResponse)
+    private fun getAllLibrariesInResponseFormat(): Mono<List<Library>> =
+        adminService.getAllLibraries().map(LibraryDtoResponse::toLibraryResponse).collectList()
 
-    private fun createSuccessResponse(librariesResponse: List<Library>) =
+    private fun createSuccessResponse(librariesResponse: List<Library>): GetAllLibrariesResponse =
         GetAllLibrariesResponse.newBuilder().apply {
             successBuilder.addAllLibraries(librariesResponse)
         }.build()
--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/controller/admin/GetAllUsersNatsController.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/controller/admin/GetAllUsersNatsController.kt
@@ -2,9 +2,9 @@ package systems.ajax.englishstudytelegrambot.nats.controller.admin
 
 import com.google.protobuf.Parser
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import systems.ajax.NatsSubject.Admin.GET_ALL_USERS_SUBJECT
 import systems.ajax.englishstudytelegrambot.dto.entity.UserDtoResponse
-import systems.ajax.englishstudytelegrambot.entity.User
 import systems.ajax.englishstudytelegrambot.nats.controller.NatsController
 import systems.ajax.englishstudytelegrambot.service.AdminService
 import systems.ajax.response_request.admin.GetAllUsersRequest
@@ -19,15 +19,13 @@ class GetAllUsersNatsController(
 
     override val parser: Parser<GetAllUsersRequest> = GetAllUsersRequest.parser()
 
-    override fun handle(request: GetAllUsersRequest): GetAllUsersResponse =
-        runCatching {
-            val telegramUserIds: List<String> = getTelegramUserIds()
-            createSuccessResponse(telegramUserIds)
-        }.getOrElse {
-            createFailureResponse(it)
-        }
+    override fun handle(request: GetAllUsersRequest): Mono<GetAllUsersResponse> =
+        getTelegramUserIds()
+            .map { createSuccessResponse(it) }
+            .onErrorResume { Mono.just(createFailureResponse(it)) }
 
-    private fun getTelegramUserIds(): List<String> = adminService.getAllUsers().map(UserDtoResponse::telegramUserId)
+    private fun getTelegramUserIds(): Mono<List<String>> =
+        adminService.getAllUsers().map(UserDtoResponse::telegramUserId).collectList()
 
     private fun createSuccessResponse(telegramUserIds: List<String>) =
         GetAllUsersResponse.newBuilder().apply {
