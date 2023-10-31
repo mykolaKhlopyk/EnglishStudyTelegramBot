8e5dbf7ef408a1d017f3270126745e05dca83560
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-10-31 19:23:09 +0200

    create bean ReactiveRedisTemplate

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/config/RedisConfiguration.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/config/RedisConfiguration.kt
@@ -1,11 +1,25 @@
 package systems.ajax.englishstudytelegrambot.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.*
+import systems.ajax.englishstudytelegrambot.entity.Word
+
 
 @Configuration
 class RedisConfiguration {
 
+    @Bean
+    fun reactiveRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, Word> {
+        val keySerializer = StringRedisSerializer()
+        val objectMapper = ObjectMapper().findAndRegisterModules()
+        val valueSerializer = Jackson2JsonRedisSerializer(objectMapper, Word::class.java)
+        val context = RedisSerializationContext.newSerializationContext<String, Word>(keySerializer)
+            .value(valueSerializer)
+            .build()
+        return ReactiveRedisTemplate(factory, context)
+    }
 }
