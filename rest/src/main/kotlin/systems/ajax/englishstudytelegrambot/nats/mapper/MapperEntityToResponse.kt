f0d1f05968c9d29542463dd7fe36b94dc5604b3d
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-02 16:57:55 +0200

    update mapper and protos for entity

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/mapper/MapperEntityToResponse.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/nats/mapper/MapperEntityToResponse.kt
@@ -1,37 +1,30 @@
 package systems.ajax.englishstudytelegrambot.nats.mapper
 
-import systems.ajax.englishstudytelegrambot.dto.entity.AdditionalInfoAboutWordDtoResponse
-import systems.ajax.englishstudytelegrambot.dto.entity.LibraryDtoResponse
-import systems.ajax.englishstudytelegrambot.dto.entity.WordDtoResponse
-import systems.ajax.englishstudytelegrambot.dto.entity.toDtoResponse
 import systems.ajax.englishstudytelegrambot.entity.Library as MongoLibrary
 import systems.ajax.englishstudytelegrambot.entity.Word as MongoWord
+import systems.ajax.englishstudytelegrambot.entity.AdditionalInfoAboutWord as MongoAdditionalInfoAboutWord
 import systems.ajax.entity.LibraryOuterClass.Library
 import systems.ajax.entity.WordOuterClass.AdditionalInfoAboutWord
 import systems.ajax.entity.WordOuterClass.Word
 
-fun LibraryDtoResponse.toLibraryResponse(): Library =
-    Library.newBuilder()
-        .setId(id.toHexString())
-        .setName(name)
-        .build()
-
-fun WordDtoResponse.toWordResponse(): Word =
+fun MongoWord.toWordResponse(): Word =
     Word.newBuilder()
         .setId(id.toHexString())
         .setSpelling(spelling)
         .setTranslating(translate)
-        .setAdditionInfoAboutWord(buildAdditionalInfoAboutWord(additionalInfoAboutWordDtoResponse))
+        .setLibraryId(libraryId.toHexString())
+        .setAdditionInfoAboutWord(buildAdditionalInfoAboutWord(additionalInfoAboutWord))
         .build()
 
-fun MongoWord.toWordResponse(): Word =
-    toDtoResponse().toWordResponse()
-
 fun MongoLibrary.toLibraryResponse(): Library =
-    toDtoResponse().toLibraryResponse()
+    Library.newBuilder()
+        .setId(id.toHexString())
+        .setName(name)
+        .setOwnerId(ownerId)
+        .build()
 
-private fun buildAdditionalInfoAboutWord(additionalInfoAboutWordDtoResponse: AdditionalInfoAboutWordDtoResponse) =
-    additionalInfoAboutWordDtoResponse.let {
+private fun buildAdditionalInfoAboutWord(additionalInfoAboutWord: MongoAdditionalInfoAboutWord) =
+    additionalInfoAboutWord.let {
         AdditionalInfoAboutWord.newBuilder()
             .setDefinitionOfWord(it.definitionOfWord)
             .setPronunciationOfWord(it.pronunciationOfWord)
