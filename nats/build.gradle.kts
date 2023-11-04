7b6577c55a7b7485f87adac25a75c29487c54896
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-04 22:02:56 +0200

    add dependencies for grpc

--- nats/build.gradle.kts
+++ nats/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     id("java")
 }
@@ -7,12 +9,6 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation("com.google.protobuf:protobuf-java:3.24.4")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
     id("java-test-fixtures")
+    id("com.google.protobuf") version "0.9.4"
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.10"
@@ -14,14 +15,16 @@ dependencies {
     testFixturesApi("org.mockito:mockito-core:3.5.13")
     testFixturesApi("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
     testFixturesApi("org.springframework.boot:spring-boot-starter-data-mongodb")
+
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+
     implementation(project(":nats"))
+    implementation("io.projectreactor:reactor-core:3.5.11")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.nats:jnats:2.16.14")
-    implementation("com.google.protobuf:protobuf-java:3.24.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-webflux:3.1.3")
@@ -31,10 +34,12 @@ dependencies {
     implementation("io.lettuce:lettuce-core:6.2.6.RELEASE")
     implementation("org.springframework.data:spring-data-redis:3.1.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
     testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("io.projectreactor:reactor-test:3.5.11")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     api("org.apache.commons:commons-lang3:3.9")
 }
 
