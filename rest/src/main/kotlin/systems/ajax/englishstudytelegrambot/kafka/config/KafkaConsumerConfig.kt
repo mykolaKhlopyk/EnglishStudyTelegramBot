0a9c2cc6b6b0f96dca47e202bbd220e4a1b96754
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-08 15:42:48 +0200

    change consumer

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/kafka/config/KafkaConsumerConfig.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/kafka/config/KafkaConsumerConfig.kt
@@ -1,40 +1,49 @@
 package systems.ajax.englishstudytelegrambot.kafka.config
 
-import com.squareup.wire.get
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializerConfig
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import systems.ajax.response_request.word.EventUpdateWordOuterClass.EventUpdateWord
-import java.util.*
+
 
 @EnableKafka
 @Configuration
-class KafkaConsumerConfig {
-
+class KafkaConsumerConfig(
     @Value("\${spring.kafka.bootstrap-servers}")
-    lateinit var bootstrapServers: String
+    val bootstrapServers: String,
+    @Value("\${spring.kafka.schema-registry-url}")
+    val schemaRegistryUrl: String
+) {
 
     @Bean
-    fun consumer(): KafkaConsumer<String, EventUpdateWord> {
-        val consumer =  KafkaConsumer<String, EventUpdateWord>(getProperties())
-        consumer.subscribe(listOf("mytopic"))
-        return consumer
+    fun consumerFactory(): ConsumerFactory<String, EventUpdateWord> {
+        return DefaultKafkaConsumerFactory(getMapProperties())
     }
 
-    fun getProperties(): Properties =
-        Properties().apply {
-            set(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,  bootstrapServers)
-            set(ConsumerConfig.GROUP_ID_CONFIG,"group_id")
-            set(KafkaProtobufDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8085")
-            set(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-            set(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer::class.java)
-            set(KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE, EventUpdateWord::class.java)
-        }
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, EventUpdateWord> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, EventUpdateWord>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+
+    private fun getMapProperties(): MutableMap<String, Any> =
+        mutableMapOf(
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.GROUP_ID_CONFIG to "group_id",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaProtobufDeserializer::class.java,
+            KafkaProtobufDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl,
+            KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE to EventUpdateWord::class.java
+        )
 }
