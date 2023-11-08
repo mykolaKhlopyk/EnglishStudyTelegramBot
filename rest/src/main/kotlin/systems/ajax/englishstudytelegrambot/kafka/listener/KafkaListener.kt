3313dec0de7d14b55edf0edbb2e6d2061a73acd0
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-08 15:43:14 +0200

    update listener

--- rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/kafka/listener/KafkaListener.kt
+++ rest/src/main/kotlin/systems/ajax/englishstudytelegrambot/kafka/listener/KafkaListener.kt
@@ -3,24 +3,20 @@ package systems.ajax.englishstudytelegrambot.kafka.listener
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.messaging.MessageHeaders
 import org.springframework.stereotype.Component
+import systems.ajax.englishstudytelegrambot.kafka.config.KafkaTopics
 import systems.ajax.response_request.word.EventUpdateWordOuterClass.EventUpdateWord
 
 @Component
 class KafkaListener {
 
     @KafkaListener(
-        topics = ["mytopic"],
-        groupId = "group_id",
-//contentTypeConverter = Serializer
+        topics = [KafkaTopics.UPDATED_WORD],
+        groupId = "group_id"
     )
     fun listener(record: ConsumerRecord<String, EventUpdateWord>) {
-        // Log the message and its class
-        // Log the message and its class
-        log.info("Received message: ${record.value().javaClass}")
-        log.info("word with spelling '{}', was updated, new translate '{}'", record.key(), record.value())
-        log.info("success '{}'", record.value().wordSpelling)
+        val message = record.value()
+        log.info("word with spelling '{}', was updated, new translate '{}'", message.wordSpelling, message.newWordTranslate)
     }
 
     companion object {
